ff0898968372d72937f3c8f024f7f8ab720c75f1
Fiko767 <mcfiko7676@gmail.com>
2025-04-06 18:00:09 +0200

    fix: corrected file name for telescope plugin

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,34 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-file-browser.nvim"
+    },
+    config = function()
+        require("telescope").setup({
+            defaults = {
+                prompt_prefix = "> ",
+                sorting_strategy = "ascending",
+                layout_strategy = "horizontal",
+                layout_config = { width = 0.75, preview_cutoff = 120 },
+                extensions = {
+                    file_browser = {}
+                },
+                pickers = {
+                    find_files = {
+                        hidden = true
+                    }
+                }
+            },
+        })
+
+        require("telescope").load_extension "file_browser"
+
+        map("n", "<leader>ff", ":Telescope find_files<CR>")
+        map("n", "<leader>fg", ":Telescope git_files<CR>")
+        map("n", "<leader>fb", ":Telescope buffers<CR>")
+        map("n", "<leader>fr", ":Telescope live_grep<CR>")
+        map("n", "<leader>fh", ":Telescope help_tags<CR>")
+        map("n", "<leader>fd", ":Telescope file_browser<CR>")
+    end,
+}
