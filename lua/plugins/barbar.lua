a42ae6d9faf694cd0d6f0d815bd02418c9b5ff3f
Fiko767 <mcfiko7676@gmail.com>
2025-04-09 16:10:20 +0200

    hide bar on startup – it was distracting

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,24 +1,25 @@
 return {
-    "romgrk/barbar.nvim",
-    dependencies = {
-        "lewis6991/gitsigns.nvim", -- OPTIONAL
-        "nvim-tree/nvim-web-devicons", -- OPTIONAL
-    },
-    init = function()
-        vim.g.barbar_auto_setup = false
-    end,
-    config = function()
-        -- Setup aufrufen, sonst kommt keine Leiste!
-        require("barbar").setup({
-            animation = true,
-            auto_hide = false,
-            -- Weitere Optionen hier …
-        })
+	"romgrk/barbar.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"lewis6991/gitsigns.nvim", -- OPTIONAL
+		"nvim-tree/nvim-web-devicons", -- OPTIONAL
+	},
+	init = function()
+		vim.g.barbar_auto_setup = false
+	end,
+	config = function()
+		-- Setup aufrufen, sonst kommt keine Leiste!
+		require("barbar").setup({
+			animation = true,
+			auto_hide = false,
+			-- Weitere Optionen hier …
+		})
 
-        map("n", "<A-h>", "<Cmd>BufferPrevious<CR>")
-        map("n", "<A-l>", "<Cmd>BufferNext<CR>")
-        map("n", "<A-,>", "<Cmd>BufferMovePrevious<CR>")
-        map("n", "<A-.>", "<Cmd>BufferMoveNext<CR>")
-        map("n", "<A-q>", "<Cmd>BufferClose<CR>")
-    end,
+		map("n", "<A-h>", "<Cmd>BufferPrevious<CR>")
+		map("n", "<A-l>", "<Cmd>BufferNext<CR>")
+		map("n", "<A-,>", "<Cmd>BufferMovePrevious<CR>")
+		map("n", "<A-.>", "<Cmd>BufferMoveNext<CR>")
+		map("n", "<A-q>", "<Cmd>BufferClose<CR>")
+	end,
 }
