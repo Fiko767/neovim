47b507073783dd1d5000dab2fa9d96e2b03434b6
Fiko767 <767fiko@gmail.com>
2025-04-11 19:59:02 +0200

    style: formatted all the files

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,27 +1,26 @@
 return {
-    "williamboman/mason.nvim", -- Basic config, can be expanded later
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    },
-    config = function()
-        require("mason").setup()
+	"williamboman/mason.nvim", -- Basic config, can be expanded later
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason").setup()
 
-        require("mason-lspconfig").setup({
-            ensure_installed = {},
-            automatic_installation = true,
-        })
+		require("mason-lspconfig").setup({
+			ensure_installed = {},
+			automatic_installation = true,
+		})
 
-        require("mason-lspconfig").setup_handlers({
-            function(server_name)
-                local lspconfig = require("lspconfig")
-                local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				local lspconfig = require("lspconfig")
+				local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-                lspconfig[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
-        })
-    end,
+				lspconfig[server_name].setup({
+					capabilities = capabilities,
+				})
+			end,
+		})
+	end,
 }
-
--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,7 +1,7 @@
 return {
 	"rcarriga/nvim-notify",
-	config = function ()
+	config = function()
 		vim.notify = require("notify")
 		require("notify").setup()
-	end
+	end,
 }
