4edb349b99f5ca421e17df42ee5131111cc8e83d
Fiko767 <767fiko@gmail.com>
2025-04-11 15:19:39 +0200

    feat: added cmp.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,27 @@
 return {
-	"williamboman/mason.nvim", -- It's a very basic config later it will be more
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	},
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup()
-	end,
+    "williamboman/mason.nvim", -- Basic config, can be expanded later
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("mason").setup()
+
+        require("mason-lspconfig").setup({
+            ensure_installed = {},
+            automatic_installation = true,
+        })
+
+        require("mason-lspconfig").setup_handlers({
+            function(server_name)
+                local lspconfig = require("lspconfig")
+                local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+                lspconfig[server_name].setup({
+                    capabilities = capabilities,
+                })
+            end,
+        })
+    end,
 }
+
