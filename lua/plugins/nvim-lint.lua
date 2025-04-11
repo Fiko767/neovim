72c2f9bc3dbfeb0da4f48c114c44477a4b262351
Fiko767 <767fiko@gmail.com>
2025-04-11 21:46:32 +0200

    feat: add linting

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,6 +1,11 @@
 return {
 	"mfussenegger/nvim-lint",
 	config = function()
-		require("lint").linters_by_ft = {}
+		require("lint").linters_by_ft = {
+			lua = { "luacheck" },
+			python = { "flake8" },
+			rust = { "bacon" },
+			javascript = { "eslint_d" },
+		}
 	end,
 }
