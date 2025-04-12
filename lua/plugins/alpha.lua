59ea82ad350306cb9b47802a24a6a0aa46b3986e
Fiko767 <767fiko@gmail.com>
2025-04-12 08:51:44 +0200

    feat: added a dashboard

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,31 @@
+return {
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+			[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+			[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+			[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+			[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+			[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", "<cmd>ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button("l", "󰒲 Lazy.nvim", "<cmd>Lazy<CR>"),
+			dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+		}
+
+		dashboard.section.footer.val = { "Happy coding with Neovim!" }
+
+		alpha.setup(dashboard.opts)
+	end,
+}
