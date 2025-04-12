c98ad6c53dbc032c00b3128652764387e121c59e
Fiko767 <767fiko@gmail.com>
2025-04-12 19:45:38 +0200

    chore(nvim-tree): extend the config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,6 +4,40 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup()
+		require("nvim-tree").setup({
+			sync_root_with_cwd = true,
+			filters = {
+				dotfiles = false,
+			},
+			git = {
+				enable = true,
+				ignore = false,
+			},
+			renderer = {
+				indent_markers = {
+					enable = true,
+					icons = {
+						corner = "└",
+						edge = "│",
+						item = "├",
+						none = " ",
+					},
+				},
+				highlight_git = "none",
+				icons = {
+					glyphs = {
+						git = {
+							unstaged = "",
+							staged = "",
+							unmerged = "",
+							renamed = "",
+							untracked = "",
+							deleted = "",
+							ignored = "󰴲",
+						},
+					},
+				},
+			},
+		})
 	end,
 }
