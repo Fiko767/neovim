5823714bc14c456c14e92851e0ab5093772ff835
Fiko767 <767fiko@gmail.com>
2025-04-16 11:20:06 +0200

    fix: were annoying

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,7 +3,6 @@ vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
 vim.keymap.set("n", "<leader>x", "<cmd>wqa<CR>")
 
 vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("v", "jj", "<Esc>")
 
 vim.keymap.set("n", "<leader>t", "<cmd>terminal<CR>")
 
@@ -14,10 +13,6 @@ vim.keymap.set("i", "<C-k>", "<Up>")
 vim.keymap.set("i", "<C-l>", "<Right>")
 
 vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>")
-
--- Split and Vsplit
-vim.keymap.set("n", "<leader>sh", "<cmd>split<CR>")
-vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<CR>")
 
 -- LSP (from ChatGPT)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
