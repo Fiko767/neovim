6d2099039bfe0c6596b9e3004355b8fa21ca6164
Fiko767 <767fiko@gmail.com>
2025-04-20 15:21:12 +0200

    feat(keymaps): add keymaps for lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,73 @@
+return {
+	"williamboman/mason.nvim", -- Basic config, can be expanded later
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	-- from AI
+	keys = {
+		{ "K", vim.lsp.buf.hover, desc = "Hover Documentation" },
+		{ "gd", vim.lsp.buf.definition, desc = "Go to Definition" },
+		{ "gD", vim.lsp.buf.declaration, desc = "Go to Declaration" },
+		{ "gi", vim.lsp.buf.implementation, desc = "Go to Implementation" },
+		{ "gr", vim.lsp.buf.references, desc = "Go to References" },
+		{ "<leader>ls", vim.lsp.buf.signature_help, desc = "Signature Help" },
+
+		{ "<leader>la", vim.lsp.buf.code_action, desc = "Code Action" },
+		{ "<leader>lr", vim.lsp.buf.rename, desc = "Rename Symbol" },
+		{ "gT", vim.lsp.buf.type_definition, desc = "Go to Type Definition" },
+
+		{ "<leader>lS", vim.lsp.buf.workspace_symbol, desc = "Workspace Symbols" },
+		{ "<leader>ld", vim.lsp.buf.document_symbol, desc = "Document Symbols" },
+
+		{ "[d", vim.diagnostic.goto_prev, desc = "Previous Diagnostic" },
+		{ "]d", vim.diagnostic.goto_next, desc = "Next Diagnostic" },
+		{ "<leader>le", vim.diagnostic.open_float, desc = "Show Line Diagnostics" },
+		{ "<leader>lq", vim.diagnostic.setqflist, desc = "Diagnostic Quickfix List" },
+	},
+	config = function()
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"luacheck",
+				"pylint",
+				"bacon",
+				"eslint_d",
+				"stylelint",
+				"cpplint",
+				"golangci-lint",
+				"markdownlint",
+			},
+			auto_update = true,
+		})
+
+		require("mason").setup()
+
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+				"rust_analyzer",
+				"ts_ls",
+				"html",
+				"cssls",
+				"clangd",
+				"yamlls",
+				"gopls",
+				"marksman",
+			},
+			automatic_installation = true,
+		})
+
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				local lspconfig = require("lspconfig")
+				local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+				lspconfig[server_name].setup({
+					capabilities = capabilities,
+				})
+			end,
+		})
+	end,
+}
