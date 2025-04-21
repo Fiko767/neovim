56cd431e8051d57ba1438c9806525c2ee174697e
Fiko767 <767fiko@gmail.com>
2025-04-21 21:21:51 +0200

    forgot to change it

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,9 +1,9 @@
 return {
 	require("plugins.lsp.cmp"),
 	require("plugins.lsp.conform"),
-	require("plugins.lsp.lsp"),
 	require("plugins.lsp.lspsaga"),
 	require("plugins.lsp.luasnip"),
+	require("plugins.lsp.mason"),
 	require("plugins.lsp.nvim-lint"),
 	require("plugins.lsp.tiny-inline-diagnostic"),
 }
