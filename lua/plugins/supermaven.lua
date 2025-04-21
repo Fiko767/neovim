8cc4070e581faad1205662bc73ee1d253456b0b2
Fiko767 <767fiko@gmail.com>
2025-04-21 09:37:25 +0200

    feat(supermaven): add keymaps for supermaven

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -5,6 +5,12 @@ return {
 		{ "<leader>St", "<cmd>SupermavenToggle<CR>" },
 	},
 	config = function()
-		require("supermaven-nvim").setup({})
+		require("supermaven-nvim").setup({
+			keymaps = {
+				accept_suggestion = "<Tab>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C-s>",
+			},
+		})
 	end,
 }
