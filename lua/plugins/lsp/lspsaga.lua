ce92035dcd89dad32b96e4114cdd8e06e75a53c2
Fiko767 <767fiko@gmail.com>
2025-04-26 14:19:20 +0200

    removed the lightbulb from lspsaga

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -2,20 +2,16 @@ return {
 	"nvimdev/lspsaga.nvim",
 	event = "VeryLazy",
 	keys = {
-		-- Hover Docs
 		{ "K", "<cmd>Lspsaga hover_doc<CR>", desc = "Hover Documentation" },
 
-		-- Definitionen und Referenzen
 		{ "gd", "<cmd>Lspsaga goto_definition<CR>", desc = "Go to Definition" },
 		{ "gi", "<cmd>Lspsaga finder imp<CR>", desc = "Go to Implementation" },
 		{ "gr", "<cmd>Lspsaga finder ref<CR>", desc = "Go to References" },
 		{ "gT", "<cmd>Lspsaga goto_type_definition<CR>", desc = "Go to Type Definition" },
 
-		-- Code Actions & Rename
 		{ "<leader>la", "<cmd>Lspsaga code_action<CR>", desc = "Code Action" },
 		{ "<leader>lr", "<cmd>Lspsaga rename<CR>", desc = "Rename Symbol" },
 
-		-- Diagnostics
 		{ "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Previous Diagnostic" },
 		{ "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next Diagnostic" },
 		{ "<leader>le", "<cmd>Lspsaga show_line_diagnostics<CR>", desc = "Show Line Diagnostics" },
@@ -23,6 +19,10 @@ return {
 		{ "<leader>lo", "<cmd>Lspsaga outline<CR>", desc = "Outline" },
 	},
 	config = function()
-		require("lspsaga").setup()
+		require("lspsaga").setup({
+			lightbulb = {
+				enable = false,
+			},
+		})
 	end,
 }
